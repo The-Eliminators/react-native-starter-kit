8e5686eb461019d4be22affa6bd0e0d4ed2cff89
Sanket Hebbal <sanket.hebbal@springct.com>
2021-07-31 18:53:13 +0530

    Added splash screen with theme support on ios.

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 import { enableScreens } from 'react-native-screens';
 enableScreens(true);
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ThemeProvider } from '@shopify/restyle';
 import { useColorScheme } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -12,8 +12,15 @@ import theme from 'src/theme';
 import Navigation from 'src/navigation';
 import { SnackbarProvider } from 'src/context';
 
+import SplashScreen from 'react-native-splash-screen';
+
 const App = () => {
   const themeMode = useColorScheme();
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <ThemeProvider theme={themeMode === 'light' ? theme.lightTheme : theme.darkTheme}>
       <SafeAreaProvider>
---
+++ android/app/src/main/java/com/reactnativestarterkit/SplashScreenActivity.java
@@ -0,0 +1,13 @@
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+
+    }
+}
