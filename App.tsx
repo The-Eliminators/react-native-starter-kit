ced6d855e263e9a4b166fd6ee425563423e045cd
Rohit Kadam <rohitkadam1407@gmail.com>
2022-04-03 20:15:45 +0530

    Added splash screen with theme supporton ios

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 import { enableScreens } from 'react-native-screens';
 enableScreens(true);
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import { ThemeProvider } from '@shopify/restyle';
 import { useColorScheme } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -17,15 +17,16 @@ import SplashScreen from 'react-native-splash-screen';
 const App = () => {
   const themeMode = useColorScheme();
 
-  useEffect(() => {
-    SplashScreen.hide();
-  }, []);
+  // Check when react navigation is ready
+  const onNavigationReady = () => {
+    SplashScreen.hide(); // hide the splash screen after navigation ready
+  };
 
   return (
     <ThemeProvider theme={themeMode === 'light' ? theme.lightTheme : theme.darkTheme}>
       <SafeAreaProvider>
         <SnackbarProvider>
-          <Navigation />
+          <Navigation onReady={onNavigationReady} />
         </SnackbarProvider>
       </SafeAreaProvider>
     </ThemeProvider>
